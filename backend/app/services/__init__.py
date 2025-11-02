2073f529d6f99c3726a0608dbfabba0ef8b6f6e7
Jayden Stenfort <jss8102@rit.edu>
2025-11-01 20:26:58 -0400

    Implement basic tui from scratch

--- backend/app/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Services package initialization."""
-
-from .scraping_service import ScrapingService
-from .claude_service import ClaudeService
-from .media_service import MediaService
-
-__all__ = ['ScrapingService', 'ClaudeService', 'MediaService']
--- database_handler/config/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Configuration module for database handler."""
-
-from .database import DatabaseConfig
-from .settings import load_config_from_env, load_config_from_file
-
-__all__ = ["DatabaseConfig", "load_config_from_env", "load_config_from_file"]
